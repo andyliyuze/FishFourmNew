bd98d7f716be7488e8216276d88eee6aa230780b
andyliyuze <874787504@qq.com>
2017-12-08 10:27:01 +0800

    大

--- FishFourm.Test/Posts/PostAppService_Tests.cs
+++ FishFourm.Test/Posts/PostAppService_Tests.cs
@@ -10,12 +10,13 @@ using FishFourm.Application.Posts.Dtos;
 using NSubstitute;
 using AutoMapper;
 using FishFourm.Core.Repository;
+using FishFourm.EntityFramework.Repository;
 
 namespace FishFourm.Test.Posts
 {
     public class PostAppService_Tests : FishFourmTestBase
     {
-        private readonly IPostAppService _postAppService;
+        private   IPostAppService _postAppService;
         private readonly IUserRepository _userRepositoryMock;
         private readonly IPostRepository _postRepositoryMock;
 
@@ -57,11 +58,10 @@ namespace FishFourm.Test.Posts
            
         }
 
+        //测试交互，就是测试方法有没有没争取调用
         [Fact]
         public async Task CreatePost_Test()
         {
-            //Arrange
-            var countBefore = UsingDbContext(a => a.Post.Count());
             CreatePostDto postDto = new CreatePostDto()
             {
                 AuthorId = UsingDbContext<User>(a => a.User.FirstOrDefault()).Id,
@@ -69,21 +69,49 @@ namespace FishFourm.Test.Posts
                 Title = "title5"
             };
             var post = new Post(postDto.AuthorId, postDto.Title, postDto.Content);
-            _postRepositoryMock.InsertAsync(post).Returns(Arg.Is<Post>(a=>a.Title==post.Title && a.AuthorId == post.AuthorId),post);
 
+            //只要InsertAsync方法接收到这些参数值就返回post实例
+            _postRepositoryMock.InsertAsync(Arg.Is<Post>(
+            a => a.Content == post.Content
+            && a.AuthorId == post.AuthorId
+            && a.Title == post.Title)).Returns(post);
+
+            //Act
+            var flag = await _postAppService.CreatePost(postDto);
+
+
+            //Assert
+            //只要InsertAsync方法被调用，而且接收到参数值符合预期就断言通过
+            await _postRepositoryMock.Received().InsertAsync(Arg.Is<Post>( 
+            a=>a.Content==post.Content
+            &&a.AuthorId==post.AuthorId
+            &&a.Title==post.Title));
+            
+        }
+
+        //测试结果
+        [Fact]
+        public async Task CreatePost_Test_Count_Should_Increment()
+        {
+            //Arrange
+             _postAppService = LocalIocManager.Resolve<IPostAppService>();
+            var countBefore = UsingDbContext(a => a.Post.Count());
+            CreatePostDto postDto = new CreatePostDto()
+            {
+                AuthorId = UsingDbContext<User>(a => a.User.FirstOrDefault()).Id,
+                Content = "HI",
+                Title = "title5"
+            };
+          
             //Act
             var flag = await _postAppService.CreatePost(postDto);
             var count = UsingDbContext(a => a.Post.Count());
 
-            //Assert
-            await _postRepositoryMock.Received().InsertAsync(Arg.Is<Post>( 
-            a=>a.Content==post.Content
-            &&a.AuthorId==post.AuthorId
-            &&a.Title==post.Title));       
-            
+            //Assert       
             Assert.True(flag);
             Assert.Equal(countBefore + 1, count);
         }
+
 
         [Fact]
         public async Task GetPostsByAuthorId_Test()
