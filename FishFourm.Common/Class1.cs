92c84a08f8956b317aca290071709d4763a7c168
andyliyuze <874787504@qq.com>
2018-01-10 18:04:00 +0800

    xiugai1

--- FishFourm.Common/Class1.cs
+++ FishFourm.Common/Class1.cs
@@ -1,13 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace FishFourm.Common
 {
-    public class PostInput
-    {     
-            public Guid AuthorId { get; set; }
-
-            public string Title { get; set; }
-
-            public string Content { get; set; }
+    public class Class1
+    {
     }
 }
